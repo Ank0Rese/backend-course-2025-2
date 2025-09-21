775a8ad82888b37df43ef1275adae2ddcf934f2e
Юрій bc2025-2 <equipment22360@gmail.com>
2025-09-22 01:23:20 +0300

    Написав програму, яка буде конструювати URL для запиту з частини 2

---
+++ main.js
@@ -0,0 +1,19 @@
+const { URL } = require('url');
+
+const currency = 'usd';
+const date_from = '20220707';
+const date_to = '20220719';
+
+const baseUrl = 'https://bank.gov.ua';
+const apiPath = '/NBU_Exchange/exchange_site';
+
+const url = new URL(apiPath, baseUrl);
+
+url.searchParams.append('start', date_from);
+url.searchParams.append('end', date_to);
+url.searchParams.append('valcode', currency);
+url.searchParams.append('sort', 'exchangedate');
+url.searchParams.append('order', 'desc');
+url.searchParams.append('json', '');
+
+console.log(url.toString());
